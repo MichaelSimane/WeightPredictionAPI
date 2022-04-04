9670ee39cd3825d7d7345f18d1bb119abe8121b1
mickeybs <mickeysimanemb@gmail.com>
2022-04-04 11:17:22 +0300

    first commit

---
+++ api/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+from django.conf import settings
+import joblib
+import os
+
+
+class ApiConfig(AppConfig):
+    name = 'api'
+    MODEL_FILE = os.path.join(settings.MODELS, "WeightPredictionLinRegModel.joblib")
+    model = joblib.load(MODEL_FILE)
